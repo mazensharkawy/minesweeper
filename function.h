bb7f4f99ea6ea43a1c0a79d8ac9cbb8b23cde57d
mazensharkawy <mazenmaher1997@gmail.com>
2016-12-25 23:55:00 +0200

    Added a header File functions.h
    separated the code among 3 files : main.c, functions.c, functions.h

---
+++ function.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_H_INCLUDED
+#define FUNCTION_H_INCLUDED
+void start();
+void play();
+void count_adjacent_bombs(char** grid,int xrow,int ycol,int rows,int cols, int state);
+void lose();
+void load();
+void leaders();
+void save();
+void won();
+
+
+#endif // FUNCTION_H_INCLUDED
